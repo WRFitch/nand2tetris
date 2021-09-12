a2cd16a23f3e1111effd7c83ad94b79c000f9975
WRFitch <WRFitch@outlook.com>
2021-09-12 01:45:15 +0100

    completed project 04b - multiply numbers

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,32 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
+//EITHER loop through each one, adding it to the next
+//OR for each digit in R1, take an AND of R0, appending i 0's to the right-hand side. Sum these numbers, discarding the negative signifier. 
+
+//In a more detailed instruction set, the second idea would be better, but as it stands, bitwise work may be tricky. 
+
 // Put your code here.
+
+@R2
+M=0
+
+(MULT_LOOP)
+@R1
+D=M
+@EXIT
+D;JEQ
+
+@R0
+D=M
+
+@R2
+M=M+D
+
+@R1
+M=M-1
+
+@MULT_LOOP
+0;JMP
+
+(EXIT)
